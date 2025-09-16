abba4d16a6818aba2d5b43205d669864df880151
lilJ <julia.s@live.nl>
2025-09-16 17:43:49 +0200

    install

---
+++ src/pages/Index.tsx
@@ -0,0 +1,23 @@
+import Navigation from '../components/Navigation';
+import HeroSection from '../components/HeroSection';
+import GallerySection from '../components/GallerySection';
+import AboutSection from '../components/AboutSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <main>
+        <HeroSection />
+        <GallerySection />
+        <AboutSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
